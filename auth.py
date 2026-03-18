286c24d9f8f0796c38e2fe480031424de8f7f4e4
Diaz694 <147392752+Diaz694@users.noreply.github.com>
2026-03-18 17:33:41 +0530

    Initial commit

---
+++ auth.py
@@ -0,0 +1,32 @@
+from datetime import datetime, timedelta, timezone
+import jwt
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from passlib.context import CryptContext
+from sqlalchemy.orm import Session
+from database import get_db
+from models import User
+from f_config import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
+#secret_key = "secretkey"
+#algorithm = "HS256"
+#accesstokenexpiration = 30
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+def hash_password(password):
+    return pwd_context.hash(password)
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+def create_access_token(username):
+    expire=datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    data={"sub": username, "exp": expire}
+    return jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username = payload.get("sub")
+    except:
+        raise HTTPException(status_code=401, detail="Invalid token")
+    user = db.query(User).filter(User.username == username).first()
+    if user is None:
+        raise HTTPException(status_code=401, detail="User not found")
+    return user
---
+++ utils.py
@@ -0,0 +1,11 @@
+import time
+from functools import wraps
+def log_req(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"Function {func.__name__} Time: {end_time - start_time:.4f} seconds")
+        return result
+    return wrapper
